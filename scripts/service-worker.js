9b82a5023eb6e4a922c04fa3c633b34953125b2f
Thom Allen <thomwork@gmail.com>
2017-08-24 14:08:58 +0100

    Add basic service-worker.js

---
+++ scripts/service-worker.js
@@ -0,0 +1,13 @@
+// BASIC USE CASE ONLY
+var cacheName = 'weatherPWA-step-6-1';
+var filesToCache = [];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
